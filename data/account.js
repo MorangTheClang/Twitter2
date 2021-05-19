e87992bd08371aea42e183c1e02c8e5c97194db7
MorangTheClang <brendendude@hotmail.com>
2021-05-18 23:24:09 -0700

    account

---
+++ data/account.js
@@ -0,0 +1,10 @@
+window.YTD.account.part0 = [ {
+  "account" : {
+    "email" : "brendenonline@shaw.ca",
+    "createdVia" : "oauth:3033300",
+    "username" : "BrendenRecluse",
+    "accountId" : "1350922293367635968",
+    "createdAt" : "2021-01-17T21:45:50.193Z",
+    "accountDisplayName" : "Clergy"
+  }
+} ]
